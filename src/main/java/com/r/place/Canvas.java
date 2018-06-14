75de45253a6139e8b4b04d1cf6506631edd42f5f
Dominik Hofman <dominikhofman0@gmail.com>
2018-06-14 06:22:13 +0200

    Added pixels

--- src/main/java/com/r/place/Canvas.java
+++ src/main/java/com/r/place/Canvas.java
@@ -13,19 +13,54 @@ public class Canvas {
     private Long id;
     private Integer width;
     private Integer height;
+    int[] data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public int getPixel(int x, int y) {
+        return data[ y * getWidth() + x];
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public void setWidth(Integer width) {
+
+        this.width = width;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPixel(int x, int y, int color) {
+        data[ y * getWidth() + x] = color;
+    }
 
     protected Canvas() {}
 
     public Canvas(Integer width, Integer height) {
         this.width = width;
         this.height = height;
+        this.data = new int[width * height];
     }
 
     @Override
     public String toString() {
         return String.format(
-                "Canvas[id=%d, width='%d', height='%d']",
-                id, width, height);
+                "Canvas[id=%d, width='%d', height='%d', datalen='%d']",
+                id, width, height, data.length);
     }
 
 }
--- src/main/java/com/r/place/CanvasController.java
+++ src/main/java/com/r/place/CanvasController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Iterator;
+
 @Controller
 @RequestMapping(path="/canvas")
 public class CanvasController {
@@ -16,11 +18,11 @@ public class CanvasController {
 
     @GetMapping(path="/add")
     public @ResponseBody
-    String greeting(@RequestParam(name="width", required=false, defaultValue="1024") String width,
-                    @RequestParam(name="height", required=false, defaultValue="1024") String height, Model model) {
+    String greeting(@RequestParam(name="width", required=false, defaultValue="5") String width,
+                    @RequestParam(name="height", required=false, defaultValue="4") String height, Model model) {
         Canvas n = new Canvas(Integer.parseInt(width), Integer.parseInt(height));
         canvasRepository.save(n);
-        return "place";
+        return "added";
     }
 
     @GetMapping(path="/all")
@@ -29,4 +31,48 @@ public class CanvasController {
         return canvasRepository.findAll();
     }
 
+    @GetMapping(path="/debug")
+    public @ResponseBody Canvas getLastCanvas() {
+        // This returns a JSON or XML with the users
+        Iterable<Canvas> canvases =  canvasRepository.findAll();
+        final Iterator<Canvas> itr = canvases.iterator();
+        Canvas lastElement = itr.next();
+
+        while(itr.hasNext()) {
+            lastElement=itr.next();
+        }
+
+        return lastElement;
+    }
+
+    @GetMapping(path="/other")
+    public @ResponseBody Canvas test() {
+
+        return getLastCanvas();
+    }
+
+    @GetMapping(path="/pixel")
+    public @ResponseBody String pixel(@RequestParam(name="x") String x,
+                                      @RequestParam(name="y") String y,
+                                      @RequestParam(name="color") String color, Model model) {
+        Canvas c = this.getLastCanvas();
+        c.setPixel(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(color));
+        canvasRepository.save(c);
+        return "pixel saved";
+    }
+
+    @GetMapping(path="/show")
+    public @ResponseBody String show() {
+        Canvas c = this.getLastCanvas();
+        StringBuilder sb = new StringBuilder();
+
+        for (int x = 0; x < c.getWidth(); x++) {
+            for (int y = 0; y < c.getHeight(); y++) {
+               sb.append(String.format("(%d), ", c.getPixel(x, y)));
+            }
+            sb.append("<br>");
+        }
+
+        return sb.toString();
+    }
 }
